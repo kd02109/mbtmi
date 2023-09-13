ecb650195f43d445b28e24468e1ab76a065aad9c
JUNSEOK <kd02109@gmail.com>
2023-09-13 14:07:43 +0900

    Dep: 기본 패키지 설치 및 설정

---
+++ client/src/main.tsx
@@ -0,0 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { routers } from '@/routers/routers.tsx';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routers} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
