845ddd489b3615d01b9a45493cd5e2c7deab2f05
JUNSEOK <kd02109@gmail.com>
2023-09-13 15:15:50 +0900

    Dep: emotion css 적용
    What is this PR?
    - reset css 적용
    - emotion library 설치

---
+++ client/src/css/reset.ts
@@ -0,0 +1,23 @@
+import { css } from '@emotion/react';
+export const reset = css`
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    margin: 0;
+    font-family: sans-serif;
+  }
+  button {
+    appearance: none;
+    border: 0;
+    padding: 0;
+    background-color: transparent;
+    border-radius: 0;
+  }
+  h1 {
+    margin: 0;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,10 @@
+import { Global } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { reset } from '@/css/reset';
 import { routers } from '@/routers/routers.tsx';
 
 const queryClient = new QueryClient();
@@ -10,6 +12,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <Global styles={reset} />
       <RouterProvider router={routers} />
       <ReactQueryDevtools />
     </QueryClientProvider>
