fe20ff7bff30894fc294efa77a1996677a3d6f15
rudgnsdl06 <rudgnsdl06@gmail.com>
2023-09-21 00:31:32 +0900

    backend settings

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from domain.question import question_router
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/hello")
 def hello():
-    return {"message": "안녕하세요 mbtmi"}
+    return {"message": "hi mbtmi"}
+
+
+app.include_router(question_router.router)
