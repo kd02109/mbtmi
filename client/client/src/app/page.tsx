16519ecd9f8b0fe76b459b74831a8512dd86a04a
JUNSEOK <kd02109@gmail.com>
2023-09-22 21:38:47 +0900

    Feat: API 테스트

--- client/client/src/app/page.tsx
+++ client/client/src/app/page.tsx
@@ -1,5 +1,18 @@
-export default function Home() {
+import { getHello, getQuestion } from "@/server/getHello";
+
+export default async function Home() {
+  const data = await getHello()
+  const questions = await getQuestion()
+  console.log(data)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {data.message}
+      <ul>
+        {questions.map((item:any) => <li key={item.id}>{item.subject}
+          <time>{new Date(item.time).toString()}</time>
+          <span>{item.content}</span>
+        </li>)}
+      </ul>
+      </main>
   );
 }
---
+++ client/client/src/server/getHello.ts
@@ -0,0 +1,13 @@
+import { SERVER_URL } from "@/api/api"
+
+export const getHello =  async() => {
+    const data = await fetch(`${SERVER_URL}/hello`)
+    const json = data.json()
+    return json
+}
+
+export const getQuestion = async() => {
+    const data = await fetch(`${SERVER_URL}/api/question/list`)
+    const json = data.json()
+    return json
+}
