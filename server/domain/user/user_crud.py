ce091e6727a8c1426296b0e8fe1378f4bd55553c
rudgnsdl06 <rudgnsdl06@gmail.com>
2023-10-14 20:16:05 +0900

    server: jwt

--- server/domain/user/user_crud.py
+++ server/domain/user/user_crud.py
@@ -3,15 +3,20 @@ from sqlalchemy.orm import Session
 
 from domain.user.user_schema import UserCreate
 from models import UserTable
+from auth.auth_handler import signJWT
 
 def create_user(db: Session, user_create: UserCreate):
     print("create new user")
+    create_date = datetime.now()
+    jwt_token = signJWT(user_create.nickname, create_date)
     db_user = UserTable(gender=user_create.gender,
                         nickname=user_create.nickname,
-                        create_date=datetime.now()
+                        jwt_token=jwt_token['token'],
+                        create_date=create_date
                         )
     db.add(db_user)
     db.commit()
+    return jwt_token
 
 def get_user_mbtmi(user_id: int):
     print('mbtmi')
