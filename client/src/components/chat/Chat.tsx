03687173bd2426d880cad1a8caf7e985770f307a
kd02109 <57277708+kd02109@users.noreply.github.com>
2023-10-22 17:18:59 +0900

    Fix : CSS 수정 (#6)
    * Fix: 코드 디자인 수정
    
    * Feat: 채팅 목록 예시 페이지 작성
    
    What is this PR?
    - 기본적인 채팅 목록 페이지 생성
    
    * Feat: 채팅 화면 구현
    
    * Feat: 메시지 입력창 추가
    
    What is this PR?
    - 메시지 입력 창 추가
    
    * Fix : CSS 수정
    
    What is this PR?
    - 한줄에 길게 쓰면 텍스트 버블 깨지는 현상 수정
    - 휴대폰 화면, 폰 화면 중간 대화창 크기 수정
    - 기본 폰트 색 black으로 설정

--- client/src/components/chat/Chat.tsx
+++ client/src/components/chat/Chat.tsx
@@ -1,7 +1,9 @@
 export default function Chat({ message }: { message: string }) {
   return (
     <div className="chat-end chat">
-      <div className="chat-bubble text-black bg-yellow-400">{message}</div>
+      <div className="chat-bubble text-black bg-yellow-400 break-words">
+        {message}
+      </div>
     </div>
   );
 }
--- client/src/components/chat/ChatPage.tsx
+++ client/src/components/chat/ChatPage.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { useState } from 'react';
+
+import { useState, useRef, useEffect } from 'react';
 import Chat from '@/components/chat/Chat';
 import SpeechBubble from '@/components/chat/SpeechBubble';
 import ChatingDetailHeader from '@/components/layout/ChatingDetailHeader';
@@ -9,10 +10,18 @@ export default function ChatPage(prop: Data) {
   const { name, profile, questionMan, questionWoman } = prop;
   const [message, setMessage] = useState('');
   const [answer, setAnswer] = useState<string[]>([]);
+  const textRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (textRef.current) {
+      textRef.current.focus();
+    }
+  }, []);
+
   return (
     <section className="bg-bgChating flex w-full h-full min-h-screen max-w-xl m-auto flex-col justify-between">
       <ChatingDetailHeader name={name} number={2} profile={profile} />
-      <section className="flex flex-col px-4 my-4 gap-4 grow max-h-96 overflow-y-auto ">
+      <section className="flex flex-col px-4 my-4 gap-4 grow max-h-[500PX] max-md:max-h-96 overflow-y-auto ">
         {questionMan.map(item => (
           <SpeechBubble
             profile={profile}
@@ -31,19 +40,21 @@ export default function ChatPage(prop: Data) {
           onChange={e => {
             setMessage(e.target.value);
           }}
-          cols={30}
-          rows={5}
-          className="w-full pr-20 pl-4 py-2 focus:outline-none resize-none"
+          rows={4}
+          className="w-full pr-20 pl-4 py-2 focus:outline-none resize-none bg-white"
           autoFocus
+          ref={textRef}
         />
         <button
           disabled={message.length < 1 ? true : false}
-          className={`absolute bottom-4 right-4 border-2 border-solid px-4 py-2 rounded-lg border-yellow-400 bg-yellow-400 font-bold ${
-            message.length < 1 && 'text-gray-300 border-gray-300 bg-white'
+          className={`absolute bottom-4 right-6 border-2 border-solid px-4 py-2 rounded-lg border-yellow-400 bg-yellow-400 font-bold text-black ${
+            message.trim().length < 1 &&
+            'text-gray-300 border-gray-300 bg-white'
           }`}
           onClick={() => {
             answer.push(message);
             setMessage('');
+            if (textRef.current) textRef.current.focus();
           }}>
           전송
         </button>
