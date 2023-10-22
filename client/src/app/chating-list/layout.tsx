b9634131bc8d4cba1a42ba867353a528bf993c4d
kd02109 <57277708+kd02109@users.noreply.github.com>
2023-10-22 16:47:50 +0900

    Feat: 메시지 입력창 추가 (#5)
    * Fix: 코드 디자인 수정
    
    * Feat: 채팅 목록 예시 페이지 작성
    
    What is this PR?
    - 기본적인 채팅 목록 페이지 생성
    
    * Feat: 채팅 화면 구현
    
    * Feat: 메시지 입력창 추가
    
    What is this PR?
    - 메시지 입력 창 추가

--- client/src/app/chating-list/layout.tsx
+++ client/src/app/chating-list/layout.tsx
@@ -6,7 +6,7 @@ export default function SlugLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex w-full min-h-screen max-w-xl m-auto flex-col items-center  bg-white">
+    <main className="flex w-full h-full min-h-screen max-w-xl m-auto flex-col items-center  bg-white">
       {children}
     </main>
   );
