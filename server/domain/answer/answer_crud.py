cb224b1827a577f03db9bf537658a640d1acf983
rudgnsdl06 <rudgnsdl06@gmail.com>
2023-11-25 14:53:27 +0900

    modify: api

--- server/domain/answer/answer_crud.py
+++ server/domain/answer/answer_crud.py
@@ -17,12 +17,17 @@ def create_answer(db: Session, answer_create: AnswerCreate, token, question_id):
     return -1
 
 def get_recent_answer(db: Session, token):
-    user_data = db.query(UserTable.id, UserTable.gender, UserTable.nickname).filter(UserTable.jwt_token == token).one()
-    recent_answer = db.query(Answer.question_id, Answer.content).filter(Answer.user_id == user_data[0]).order_by(Answer.create_date).all()
-    answer_dict = defaultdict(list)
-    for i in recent_answer:
-        answer_dict[i[0]].append(i[1])
-    for i in range(1,7):
-        if i not in answer_dict.keys():
-            answer_dict[str(i)].extend([])
+    try:
+        answer_dict = defaultdict(list)
+        user_data = db.query(UserTable.id, UserTable.gender, UserTable.nickname).filter(UserTable.jwt_token == token).one()
+        recent_answer = db.query(Answer.question_id, Answer.content).filter(Answer.user_id == user_data[0]).order_by(Answer.create_date).all()
+        for i in recent_answer:
+            answer_dict[i[0]].append(i[1])
+    except Exception as e:
+        print('*'*100)
+        print(e)
+    finally:
+        for i in range(1,7):
+            if i not in answer_dict.keys():
+                answer_dict[str(i)].extend([])
     return user_data, answer_dict
