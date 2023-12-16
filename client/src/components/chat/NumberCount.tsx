f314a7317f1c5772d309e728b8786d0a9b12cdad
JUNSEOK <kd02109@gmail.com>
2023-12-16 15:08:53 +0900

    fix: 채팅 리스트 창에 현재까지 참여한 유저수 보여주기

--- client/src/components/chat/NumberCount.tsx
+++ client/src/components/chat/NumberCount.tsx
@@ -1,12 +1,16 @@
 'use client';
 import { motion, useMotionValue, animate, useTransform } from 'framer-motion';
 import { useEffect } from 'react';
-export default function NumberCount({ number }: { number: number }) {
+export default function NumberCount({
+  number,
+}: {
+  number: number | undefined;
+}) {
   const count = useMotionValue(0);
   const rounded = useTransform(count, prev => Math.floor(prev));
 
   useEffect(() => {
-    const controls = animate(count, number);
+    const controls = animate(count, number ? number : 1000);
     return controls.stop;
   }, [count, number]);
   return <motion.span className="text-red-400">{rounded}</motion.span>;
