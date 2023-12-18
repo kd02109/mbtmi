ff61e4c0123598b60cb820cbc4b66e309cd07b8d
JUNSEOK <kd02109@gmail.com>
2023-12-18 14:23:25 +0900

    feat: 이미지 다운로드 버튼에 Debounce 적용

--- client/src/hooks/useImageDownload.ts
+++ client/src/hooks/useImageDownload.ts
@@ -1,20 +1,15 @@
 'use client';
-import { useEffect, useState } from 'react';
-export default function useImageDownload(src: string, name: string) {
-  const [isDownload, setIsDownload] = useState(false);
+export default function useImageDownload(
+  src: string,
+  name: string,
+): [() => void] {
   const handleImgDownload = () => {
-    setIsDownload(true);
+    const a = document.createElement('a');
+    a.href = src;
+    a.download = name;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
   };
-  useEffect(() => {
-    if (isDownload) {
-      const a = document.createElement('a');
-      a.href = src;
-      a.download = name;
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-      setIsDownload(false);
-    }
-  }, [isDownload, src, name]);
-  return handleImgDownload;
+  return [handleImgDownload];
 }
